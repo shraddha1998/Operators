e9c852aa61f6d6cf6bc91d8e253c37401e0b4bc3
Shraddha Jadhav <32254132+shraddha1998@users.noreply.github.com>
2019-07-22 09:31:26 +0530

    Add files via upload

---
+++ hashdefine.c
@@ -0,0 +1,12 @@
+
+    #include<stdio.h>
+    #define a 5+2
+    int main()
+    {
+     int result;
+    
+      result=a*a*a*a;
+      printf("Value of a= %d\n",a);
+      printf("value of c= %d",result);
+     return 0;
+}
